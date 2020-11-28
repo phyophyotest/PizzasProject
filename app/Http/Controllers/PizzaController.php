3ec331a5e1f630fa2de4f8002b1ecdf826d4398f
aa <phyuphyuthinnpptk@gmail.com>
2020-11-28 08:33:36 +0630

    pizza data insert

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pizza;
 use Illuminate\Http\Request;
 
 class PizzaController extends Controller
@@ -21,9 +22,16 @@ class PizzaController extends Controller
            "price"=>"required",
        ]);
        if($validate){
+           $pizzas=new Pizza();
+           $pizzas->username=$res->username;
+           $pizzas->pizza_name=$res->pizza_name;
+           $pizzas->topping=$res->topping;
+           $pizzas->sauce=$res->sauce;
+           $pizzas->price=$res->price;
+           $pizzas->save();
             return back()->with("success","Thank you for your order is success!");
        }else{
-            return back($validate);
+            return back()->withErrors($validate);
        }
     }
 }
